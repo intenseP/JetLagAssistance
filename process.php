fe5d910d42a5274e211375774839772d74c97563
intenseP <125215207+intenseP@users.noreply.github.com>
2026-05-28 21:24:30 +0100

    Update process.php

--- process.php
+++ process.php
@@ -1,15 +1,19 @@
 <?php
 
 // Check if form was submitted
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
 
     // Get form values
-    $name = $_POST["name"];
-    $email = $_POST["email"];
+    $name = $_GET["name"];
+    $email = $_GET["email"];
 
     // Display the values
     echo "<h2>Form Submitted</h2>";
     echo "Name: " . htmlspecialchars($name) . "<br>";
     echo "Email: " . htmlspecialchars($email);
+}
+else{
+    echo "get shit on";
+
 }
 ?>
